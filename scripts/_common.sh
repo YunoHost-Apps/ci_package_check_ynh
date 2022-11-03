6f0039c6bc80d7e4681998539fc360cdbca128aa
yalh76 <yalh@yahoo.com>
2022-11-03 22:28:35 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ pkg_dependencies="wget git python3-pip lynx jq"
 
 ci_repository="https://github.com/YunoHost/CI_package_check"
 
-ci_release="77fbdd03ddec4f62ff46c1d7554680b3db9e69b9"
+ci_release="e6fbd469214f998db75e3c9a8904758847f38002"
 
 app_dependencies="lxd"
 
